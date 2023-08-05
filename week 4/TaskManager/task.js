d5f201cb9727abfd8e76931e83650cefb8728ff1
Gazal Sobti <gazalsobti786@gmail.com>
2023-08-05 23:19:27 +0530

    completed

--- week 4/TaskManager/task.js
+++ week 4/TaskManager/task.js
@@ -2,33 +2,34 @@ var taskInput = document.getElementById("add-task");
 var addButton = document.getElementById("additem");
 var incompletedTaskHolder = document.getElementById("incomplete");
 var completedTaskHolder = document.getElementById("completed");
-var clearButton = document.getElementById("clear");
+
 
 addButton.addEventListener("click", function () {
   var listItem = document.createElement("li");
-  var deleteButton = document.createElement("button");
+  var doneButton = document.createElement("button");
+  var text = document.createElement("span");
+  var clearButton = document.getElementById("clear");
 
-  deleteButton.innerText = "Delete";
-  deleteButton.className = "delete";
+  doneButton.innerText = "Done";
+  doneButton.className = "done";
 
-  listItem.innerText = taskInput.value;
-  listItem.appendChild(deleteButton);
+  text.innerText = taskInput.value;
+  listItem.appendChild(text);
+  listItem.appendChild(doneButton);
+  
   listItem.classList.add("itemStyling");
 
   incompletedTaskHolder.appendChild(listItem);
 
   taskInput.value = "";
-
-  listItem.addEventListener("click", function () {
+  doneButton.addEventListener("click", function () {
+    listItem.removeChild(doneButton);
     completedTaskHolder.appendChild(listItem);
     incompletedTaskHolder.removeChild(listItem);
-    listItem.style.textDecoration = "line-through";
   });
-  deleteButton.addEventListener("click", function () {
-    incompletedTaskHolder.removeChild(listItem);
+  clearButton.addEventListener("click", function () {
+    completedTaskHolder.removeChild(listItem);
   });
 });
 
-clearButton.addEventListener("click", function () {
-  completedTaskHolder.innerHTML("");
-});
+
